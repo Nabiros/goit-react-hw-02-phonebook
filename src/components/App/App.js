1d0c1f68d1db0d740f27a0abb391c92a00b7f1d2
Nazar Khizhnyak <77328105+Nabiros@users.noreply.github.com>
2021-09-10 13:30:31 +0300

    added step 1 (form)

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,13 +1,61 @@
-import "./App.css";
+import { Component } from "react";
+import { v4 as uuidv4 } from 'uuid';
+import { number } from "prop-types";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>Hi</p>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    contacts: [],
+    name: ''
+  };
+
+  nameId = uuidv4();
+  telId = uuidv4();
+
+  nameChange = (e) => {
+    const { name, value } = e.currentTarget;
+    this.setState({
+      [name]: value,
+    });
+  };
+
+  Submit = (e) => {
+    const { name, value } = this.state;
+    e.preventDefault();
+    this.props.onSubmit(name, number);
+    this.reset();
+  };
+
+  reset = () => {
+    this.setState({ name: '', number: '' });
+  };
+
+  render() {
+    return (
+      <form onSabmit={this.Submit}>
+        <h1>Phonebook</h1>
+        <label htmlFor={this.nameId}>
+          Name
+          <input
+              type="text"
+              name="name"
+              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+              title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
+            required
+            id={this.nameId}
+            value={this.state.name}
+            onChange={this.nameChange}
+          />
+          <button>add contact</button>
+        </label>
+        <h2>Contacts</h2>
+        <ul>
+          <li>Jacob</li>
+          <li>Mango</li>
+          <li>Poly</li>
+        </ul>
+        </form>
+      )
+  };
 }
 
 export default App;
