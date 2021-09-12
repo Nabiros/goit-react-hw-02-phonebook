6f5cb19b8351426741e17b88e44e65d27fd1f46b
Nazar Khizhniak <77328105+Nabiros@users.noreply.github.com>
2021-09-12 13:56:47 +0300

    finished Phonebook

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,61 +1,68 @@
 import { Component } from "react";
-import { v4 as uuidv4 } from 'uuid';
-import { number } from "prop-types";
+import { v4 as uuidv4 } from "uuid";
+import { PrimaryTitle, SecondaryTitle } from "./App.styled.jsx";
+import { ContactsForm } from "../ContactsForm/ContactsForm.jsx";
+import { ContactsList } from "../ContactsList/ContactsList.jsx";
+import { Filter } from "../Filter/Filter.jsx";
 
-class App extends Component {
+export class App extends Component {
   state = {
-    contacts: [],
-    name: ''
+    contacts: [
+      { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
+      { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
+      { id: "id-3", name: "Eden Clements", number: "645-17-79" },
+      { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
+    ],
+    filter: "",
   };
 
-  nameId = uuidv4();
-  telId = uuidv4();
+  handleFilter = (text) => {
+    this.setState({ filter: text });
+  };
 
-  nameChange = (e) => {
-    const { name, value } = e.currentTarget;
+  addContact = (name, number) => {
+    const { contacts } = this.state;
+    const newContact = {
+      id: uuidv4(),
+      name,
+      number,
+    };
+
+    contacts.some((contact) => contact.name === newContact.name)
+      ? alert(`${newContact.name} is already in contacts`)
+      : this.setState({
+          contacts: [newContact, ...contacts],
+        });
+  };
+
+  showContacts = () => {
+    const { filter, contacts } = this.state;
+    const lowerSymbol = filter.toLowerCase();
+    return contacts.filter((contact) =>
+      contact.name.toLowerCase().includes(lowerSymbol)
+    );
+  };
+
+  removeContact = (id) => {
+    const { contacts } = this.state;
     this.setState({
-      [name]: value,
+      contacts: contacts.filter((contact) => contact.id !== id),
     });
   };
 
-  Submit = (e) => {
-    const { name, value } = this.state;
-    e.preventDefault();
-    this.props.onSubmit(name, number);
-    this.reset();
-  };
-
-  reset = () => {
-    this.setState({ name: '', number: '' });
-  };
-
   render() {
-    return (
-      <form onSabmit={this.Submit}>
-        <h1>Phonebook</h1>
-        <label htmlFor={this.nameId}>
-          Name
-          <input
-              type="text"
-              name="name"
-              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-              title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
-            required
-            id={this.nameId}
-            value={this.state.name}
-            onChange={this.nameChange}
-          />
-          <button>add contact</button>
-        </label>
-        <h2>Contacts</h2>
-        <ul>
-          <li>Jacob</li>
-          <li>Mango</li>
-          <li>Poly</li>
-        </ul>
-        </form>
-      )
-  };
-}
+    const { handleFilter, addContact, showContacts, removeContact, state } =
+      this;
 
-export default App;
+    return (
+      <>
+        <PrimaryTitle>Phonebook</PrimaryTitle>
+        <ContactsForm onSubmit={addContact} />
+
+        <SecondaryTitle>Contacts</SecondaryTitle>
+        <Filter onChange={handleFilter} value={state.filter} />
+        <ContactsList contacts={showContacts()} deleteId={removeContact} />
+      </>
+    );
+  }
+}
